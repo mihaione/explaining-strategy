23166184335aea07a8db2ca184f82dd519261dc4
mihaione <mihai_ionescu@hotmail.com>
2026-04-08 22:33:41 +0300

    RS Feature

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -197,7 +197,7 @@
                                 <h1 class="sm:flex gap-8 text-2xl items-center lg:text-left sm:max-w-xl mt-8 mb-8">
                                     <div>
                                         <div class="font-lg text-blue-700 dark:text-orange-400 font-bold text-ocre">
-                                            April 7-30, 2026
+                                            A RapidStrategy feature.
                                         </div>
                                     </div>
                                     <div class="flex flex-row item-center justify-center gap-2 mt-2">
