c0d83bbc2d394fd977c0244bc2bb839c370a28e0
mihaione <mihai_ionescu@hotmail.com>
2026-05-28 16:38:00 +0300

    Editorial update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -250,7 +250,7 @@
                         A Course to Remember
                     </h2>
                     <p class="text-lg font-medium text-gray-700 dark:text-gray-300">
-                        This is not a motivational lecture. It is a course that provides a solid, practice-based, instrumental blueprint for managing the Strategy. It is the first ever Strategy course with practice sessions 100% AI-powered. How do we do this? By using the new breakthrough <a href="https://rapidstrategy.ai">RapidStrategy</a> platform.
+                        This is not a motivational lecture. It is a course that provides a solid, practice-based, instrumental blueprint for managing the Strategy. It is the first ever Strategy course with practice sessions 100% AI-powered. How do we do this? By using the <a href="https://rapidstrategy.ai">RapidStrategy</a> platform.
                     </p>
                 </div>
                 <!-- Features Row 1 -->
